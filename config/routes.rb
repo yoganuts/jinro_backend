2015e7df35d502f47802d0781ce33f0aabdd549d
Minoru Saito <u.minor3110@gmail.com>
2018-05-26 12:16:05 +0900

    changed talk routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :talks
   resources :villages do
     resources :villagers, shallow: true
+    resources :talks, ahallow: true
   end
   resources :roles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
