ef2c46ea11f8cc6a50d3da15ce8706a46ec37514
Minoru Saito <u.minor3110@gmail.com>
2018-05-26 12:48:37 +0900

    add sample seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,23 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Role.destroy_all
-Role.create(name: '村人', code: 'villager')
-Role.create(name: '人狼', code: 'beast')
+role_villager = Role.create(name: '村人', code: 'villager')
+role_beast = Role.create(name: '人狼', code: 'beast')
+
+# sample data
+Village.destroy_all
+
+10.times do |i|
+  village = Village.new(name: Faker::Zelda.location, code: SecureRandom.base64(16))
+  lottery_box = 4.times.reduce([true]) { |lot, _i| lot << false }
+  5.times do |j|
+    role = lottery_box[j] ? role_beast : role_villager
+    village.villagers.build(village: village, name: "#{Faker::Name.prefix} #{Faker::Name.last_name}", code: SecureRandom.base64(16), role: role)
+  end
+  village.villagers.each do |villager|
+    Random.rand(1..10).times do |j|
+      villager.talks.build(content: Faker::Lorem.sentence)
+    end
+  end
+  village.save
+end
