f5093818e32223a3d2ac8db15b9f24d71936d275
Minoru Saito <u.minor3110@gmail.com>
2018-05-27 21:59:30 +0900

    modified faker locale to japanese

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Faker::Config.locale = :ja
+
 Role.destroy_all
 role_villager = Role.create(name: '村人', code: :villager)
 role_beast = Role.create(name: '人狼', code: :beast)
@@ -17,7 +19,7 @@ Village.destroy_all
   lottery_box = 4.times.reduce([true]) { |lot, _i| lot << false }
   5.times do |j|
     role = lottery_box[j] ? role_beast : role_villager
-    village.villagers.build(village: village, name: "#{Faker::Name.prefix} #{Faker::Name.last_name}", code: SecureRandom.base64(16), role: role)
+    village.villagers.build(village: village, name: "#{Faker::Name.last_name} #{Faker::Name.first_name}", code: SecureRandom.base64(16), role: role)
   end
   village.villagers.each do |villager|
     Random.rand(1..10).times do |j|
