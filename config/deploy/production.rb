07761e3ca9ae9efcfc6605b76561d669e8288be0
Minoru Saito <u.minor3110@gmail.com>
2018-06-03 22:43:28 +0900

    add db role to production deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '210.129.56.158', user: 'owner', roles: %w[app web], port: 2200
+server '210.129.56.158', user: 'owner', roles: %w[app web db], port: 2200
 
 # role-based syntax
 # ==================
